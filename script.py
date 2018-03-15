8674bd16e747f35cc1166d7d55bb4d473f2f3b95
Johan Vilela <johanvilela@hotmail.com>
2018-03-15 14:23:25 -0300

    fix rebase error

--- script.py
+++ script.py
@@ -1,14 +1,9 @@
 '''
-<<<<<<< HEAD
 Comment to start the script
 '''
 
 def print_love():
     print("LOVE")
-=======
-This comment is just to start the script
-'''
 
 if __name__ == "__main__":
     print("__main__")
->>>>>>> Add __main__ at script.py
