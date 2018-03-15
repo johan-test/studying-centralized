e6dc8dddcc61360a3521c66dd7c9fa2cfbc8f55d
Johan Vilela <johanvilela@hotmail.com>
2018-03-15 14:13:43 -0300

    Add __main__ at script.py

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
 '''
+<<<<<<< HEAD
 Comment to start the script
 '''
 
 def print_love():
     print("LOVE")
+=======
+This comment is just to start the script
+'''
+
+if __name__ == "__main__":
+    print("__main__")
+>>>>>>> Add __main__ at script.py
